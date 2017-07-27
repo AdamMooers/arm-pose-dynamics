b30f8ea9f4812fb59a046bfc5e02d7513e25edcf
Adam Mooers <acm50@students.uwf.edu>
2017-07-27 13:36:45 -0500

    Adding documentation to the depth cam header.

--- depthCamManager.h
+++ depthCamManager.h
@@ -11,6 +11,7 @@
 #define DEPTHCAMMANAGER_H
 
 #include <librealsense/rs.hpp>
+#include "types.hpp"
 
 /**
  * Manages a depth camera over its lifetime. Also provides support for conversion
@@ -27,18 +28,28 @@ class depth_cam
         depth_cam * depth_cam_init();
 
         /**
-         * Converts the given depth frame into a point cloud.
+         * Converts the given depth frame into a point cloud from the camera frame of reference.
          */
 
         /**
-         * Removes the background from an image given from a known start point.
-         * The start point is in the transformed point cloud domain, so a known
-         * point cloud must be established, along 
+         * Removes the background from the source image given based on a position-based heuristic
+         * in the source image pixel domain. The resulting image is stored in the modifier
+         * buffer the is used when point cloud conversion occurs.
+         * 
+         * @param   startLoc    the normalized starting position of the algorithm. (0,0) is in the top-left
+         *                      of the image. (1,1) is the bottom-right most pixel of the image.
+         *
+         * @param   slope       the pixelwise direction to travel while searching for the start position.
+         *                      This vector is added to the current position each loop iteration.
          */
+         bool removeBackground(float startLoc, Point slope)
 
     private:
-        rs_device * dev;              // Pointer to info about the device itself
-        rs_intrinsics depth_intrin;   // Depth intrinics of the frame, updates with each new frame
+        rs_device * dev;            // Pointer to info about the device itself
+        rs_intrinsics depth_intrin; // Depth intrinics of the frame, updates with each new frame
+
+        const uint16_t * srcImg;            // A reference to the source image  
+        uint16_t * modifiedSrc = nullptr;   // A reference to the modified image
 }
 
 
